b5f5a8e6634659bdac0d5e23f3cca613c4a9a530
Administrator <gitlab_admin_360cff@example.com>
2024-07-29 15:22:18 +0330

    fix result.log

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ rm result.log
 touch result.log
 source venv/bin/activate
 pip install -r requirements.txt
-python main.py > result.txt
+python main.py > result.log
 echo "RESULT LOGS SAVED IN result.log FILE IN THIS DIRECTORY. GOOD LUCK"
